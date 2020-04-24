122e959234310bba83f285af733262d5e953880c
EvanK123 <42948168+EvanK123@users.noreply.github.com>
2020-04-24 14:10:24 -0700

    big button

--- ListSortMiniLab/UI.java
+++ ListSortMiniLab/UI.java
@@ -1,12 +1,55 @@
 import java.util.*;
-import javax.swing.*;
+import javax.swing.JFrame;
+
 import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class UI extends JFrame
 {
+	private JTextField textField;
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					UI frame = new UI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
 	public UI()
 	{
 		setResizable(false);
-		setBounds(50, 50, 1200, 800);
+		setBounds(50, 50, 600, 400);
+		getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Sort");
+		btnNewButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				CircleQueueDriver.main(null);
+			}
+		});
+		btnNewButton.setBounds(10, 329, 574, 31);
+		getContentPane().add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 11, 574, 307);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+
 	}
 }
