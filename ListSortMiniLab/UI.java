92941db5e5ff80f58c65abed4766ed8b8b02cb65
EvanK123 <42948168+EvanK123@users.noreply.github.com>
2020-04-23 18:00:08 -0700

    created UI class

---
+++ ListSortMiniLab/UI.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import javax.swing.*;
+import java.awt.BorderLayout;
+
+public class UI extends JFrame
+{
+	public UI()
+	{
+		setResizable(false);
+		setBounds(50, 50, 1200, 800);
+	}
+}
