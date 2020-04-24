cb42dc44234dc4028cb713c2f9a9650f0f73798c
Neo Argatides <neo.argatides@gmail.com>
2020-04-24 14:21:09 -0700

    ui

--- ListSortMiniLab/UI.java
+++ ListSortMiniLab/UI.java
@@ -1,11 +1,20 @@
 import java.util.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTextField;
+import java.util.Scanner;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
@@ -30,28 +39,34 @@ public class UI extends JFrame
 			}
 		});
 	}
+    private JPanel contentPane;
 	
 	public UI()
 	{
-		setResizable(false);
-		setBounds(50, 50, 600, 400);
-		getContentPane().setLayout(null);
-		
-		JButton btnNewButton = new JButton("Sort");
-		btnNewButton.addActionListener(new ActionListener()
-		{
-			public void actionPerformed(ActionEvent e) 
-			{
-				CircleQueueDriver.main(null);
-			}
-		});
-		btnNewButton.setBounds(10, 329, 574, 31);
-		getContentPane().add(btnNewButton);
-		
-		textField = new JTextField();
-		textField.setBounds(10, 11, 574, 307);
-		getContentPane().add(textField);
-		textField.setColumns(10);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 600, 500);
+        contentPane = new JPanel();
+        contentPane.setBackground(new Color(255, 100, 0));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        JButton btnTestShort = new JButton("Sort");
+        btnTestShort.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) 
+            {
+                
+            }
+        });
+        btnTestShort.setBounds(230, 443, 145, 29);
+        contentPane.add(btnTestShort);
+    
+        JTextArea result;
+        result = new JTextArea();
+        result.setEditable(false);
+        result.setColumns(10);
+        result.setBounds(6, 82, 588, 328);
+        contentPane.add(result);
 		
 
 	}
