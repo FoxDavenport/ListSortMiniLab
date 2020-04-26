348da0c405d1f363a8199dba2060951a6b26d958
MateoJGReyes <teothebulldog@gmail.com>
2020-04-25 20:30:18 -0700

    added methods needed for circlequeue with arraylist

--- ListSortMiniLab/CircleQueueAL.java
+++ ListSortMiniLab/CircleQueueAL.java
@@ -2,8 +2,56 @@ import java.util.*;
 
 public class CircleQueueAL 
 {
+	private int head;//head variable
+	private int tail;//tail variable
+	private int i;//index variable
+	private static ArrayList<Object> list = new ArrayList<Object>();//arraylist to be worked with
+	
 	public CircleQueueAL()
 	{
-		
+		head = 0;
+		tail = 0;
+		i = -1; //if the index variable was set to 0 it would mean that something already exists
+	}
+	
+	public Object getFirstObject()
+	{
+	}
+	
+	public Object getLastObject()
+	{
+	}
+	
+	public Object getObject()
+	{
+	}
+	  
+	  
+	public void setNext()
+	{
+	}
+	
+	public void setPrevious()
+	{
+	}
+	  
+	public void add(Object opaqueObject)
+	{
+	}
+	  
+	public Object delete()
+	{
+	}
+	
+	public String toString()
+	{
+	}
+	
+	public void insertionSort() 
+	{  
+	} 
+	
+	public void selectionSort() 
+	{  	  
 	}
 }
